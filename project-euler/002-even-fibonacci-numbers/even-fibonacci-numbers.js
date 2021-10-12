0f394a00a00c72b045dd520023985502cae58e7d
JCPedroza <nemesis4go10@hotmail.com>
2021-10-12 13:31:02 -0500

    refactor: integrated one function into another function

--- project-euler/002-even-fibonacci-numbers/even-fibonacci-numbers.js
+++ project-euler/002-even-fibonacci-numbers/even-fibonacci-numbers.js
@@ -1,21 +1,21 @@
-const nextFibo = (numArray) =>
+const getNextFiboNum = (numArray) =>
   numArray[numArray.length - 1] + numArray[numArray.length - 2]
 
-const appendNextFibo = (numArray) => numArray.push(nextFibo(numArray))
-
 const buildFiboRange = (limit) => {
-  const fiboRange = [1, 2]
+  const fiboRange = [0, 1]
+  let nextFiboNum = getNextFiboNum(fiboRange)
 
-  while (nextFibo(fiboRange) <= limit) {
-    appendNextFibo(fiboRange)
+  while (nextFiboNum <= limit) {
+    fiboRange.push(nextFiboNum)
+    nextFiboNum = getNextFiboNum(fiboRange)
   }
 
   return fiboRange
 }
 
-const fiboEvenSum = (limit) =>
+const fiboEvenSum = (limit = 0) =>
   buildFiboRange(limit)
     .filter(num => num % 2 === 0)
-    .reduce((acc, num) => acc + num)
+    .reduce((sum, num) => sum + num)
 
 module.exports = fiboEvenSum
