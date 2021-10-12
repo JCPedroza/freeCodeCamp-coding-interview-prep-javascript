d1790d0217058bc21e260ee3e735a9635a7555bc
JCPedroza <nemesis4go10@hotmail.com>
2021-10-12 12:48:29 -0500

    refactor according to code review se

--- rosetta-code/word-frequency/word-frequency.js
+++ rosetta-code/word-frequency/word-frequency.js
@@ -1,25 +1,21 @@
-const wordSplit = (text) =>
+const splitTextIntoWords = (text) =>
   text
-    .replace(/[.,:;?!]/g, '')
-    .split(/\s/)
+    .replace(/[.,:;?!]/g, '') // remove punctuation
+    .split(/\s/) // split into words based on whitespace
     .filter(word => word !== '')
     .map(word => word.toLowerCase())
 
-const countWords = (text) => {
-  const wordCount = {}
+const countWords = (text) =>
+  splitTextIntoWords(text).reduce((wordCount, word) => {
+    wordCount[word] ??= 0
+    wordCount[word]++
+    return wordCount
+  }, {})
 
-  const count = (word) => {
-    wordCount[word] ? wordCount[word]++ : wordCount[word] = 1
-  }
-  wordSplit(text).forEach(word => count(word))
-
-  return wordCount
-}
-
-const wordFrequency = (text = '', topn) =>
+const wordFrequency = (text = '', topN) =>
   Object
     .entries(countWords(text))
     .sort((a, b) => b[1] - a[1])
-    .slice(0, topn)
+    .slice(0, topN)
 
 module.exports = wordFrequency
